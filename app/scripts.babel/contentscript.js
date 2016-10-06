74129531dab70b5bbce0b6b0eee05a42ff9499e7
Roberto Rabasco <roberto.rabasco@just-eat.com>
2016-10-06 12:23:06 +0100

    Add info image close to every dish

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,30 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+var getelementbyclass = function (node) {
+  if (node.className == ('name ')) //filter out elements with this class attribute
+    return NodeFilter.FILTER_ACCEPT
+  else
+    return NodeFilter.FILTER_SKIP
+}
+
+function textNodesUnder(element) {
+  var node = void 0;
+  var textNodes = [];
+
+  var walker = document.createTreeWalker(element, NodeFilter.SHOW_ELEMENT, getelementbyclass, false);
+
+  while (node = walker.nextNode()) {
+    textNodes.push(node);
+  }
+
+  return textNodes;
+}
+
+var textNodes = textNodesUnder(document.body);
+
+textNodes.forEach(function (node) {
+  var imageNode = document.createElement('img');
+  imageNode.src = 'https://upload.wikimedia.org/wikipedia/commons/7/7b/Curation_bar_icon_info_16x16.png';
+  imageNode.alt = 'More  info';
+  return node.appendChild(imageNode);
+});
